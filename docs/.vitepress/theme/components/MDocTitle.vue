261031f99eee19321f7be36cd732f99fe992b8ab
nano <1174520425@qq.com>
2026-05-03 23:00:17 +0800

    feat: 实现 MDocTitle 文件名处理逻辑

--- docs/.vitepress/theme/components/MDocTitle.vue
+++ docs/.vitepress/theme/components/MDocTitle.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
 import { useData, useRoute } from 'vitepress'
+import { computed } from 'vue'
 
 const { frontmatter } = useData()
 const route = useRoute()
 
-const shouldShow = true
+const shouldShow = computed(() => {
+  // 默认显示，除非 frontmatter 中明确设置 showTitle: false
+  return frontmatter.value.showTitle !== false
+})
 
-// TODO: 实现标题处理逻辑
+const docTitle = computed(() => {
+  const path = route.path
+  // 获取路径最后一段
+  const lastSegment = path.split('/').filter(Boolean).pop() || ''
+  // 去掉扩展名
+  const nameWithoutExt = lastSegment.replace(/\.(md|html)$/, '')
+  // 替换连字符和下划线为空格
+  const withSpaces = nameWithoutExt.replace(/[-_]/g, ' ')
+  // 首字母大写
+  return withSpaces.replace(/\b\w/g, (c) => c.toUpperCase())
+})
 </script>
 
 <template>
   <div v-if="shouldShow" class="m-doc-title">
-    <!-- TODO: 显示处理后的标题 -->
+    {{ docTitle }}
   </div>
 </template>
 
