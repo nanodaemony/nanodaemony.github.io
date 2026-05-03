517218eae5e3f3fc2d5bc03ec0d533f97abd8481
nano <1174520425@qq.com>
2026-05-03 22:55:58 +0800

    feat: 创建 MDocTitle 组件骨架

---
+++ docs/.vitepress/theme/components/MDocTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useData, useRoute } from 'vitepress'
+
+const { frontmatter } = useData()
+const route = useRoute()
+
+// TODO: 实现标题处理逻辑
+</script>
+
+<template>
+  <div v-if="shouldShow" class="m-doc-title">
+    <!-- TODO: 显示处理后的标题 -->
+  </div>
+</template>
+
+<style scoped>
+/* TODO: 添加样式 */
+</style>
